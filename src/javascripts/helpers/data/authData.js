3495de435d518b715fe054b655ea9d83fde8a2f4
Silvestre Luna <slvtrluna@gmail.com>
2019-05-28 22:58:57 -0500

    added movies link

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -5,6 +5,7 @@ const authNavbar = document.getElementById('navbar-button-auth');
 const googleBtn = document.getElementById('movie-auth');
 const logoutNavbar = document.getElementById('navbar-button-logout');
 const movies = document.getElementById('all-movies');
+const navMovies = document.getElementById('navbar-button-movies');
 
 
 const checkLoginStatus = () => {
@@ -13,11 +14,13 @@ const checkLoginStatus = () => {
       authNavbar.classList.add('hide');
       googleBtn.classList.add('hide');
       logoutNavbar.classList.remove('hide');
+      navMovies.classList.remove('hide');
       movies.classList.remove('hide');
     } else {
       authNavbar.classList.remove('hide');
       googleBtn.classList.remove('hide');
       logoutNavbar.classList.add('hide');
+      navMovies.classList.add('hide');
       movies.classList.add('hide');
     }
   });
