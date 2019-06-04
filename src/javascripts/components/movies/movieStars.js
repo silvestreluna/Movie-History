b2937bbe9a54cc47d040ace60db413c94dfa4f2a
Silvestre Luna <slvtrluna@gmail.com>
2019-06-04 18:05:33 -0500

    wip

--- src/javascripts/components/movies/movieStars.js
+++ src/javascripts/components/movies/movieStars.js
@@ -3,8 +3,8 @@ import $ from 'jquery';
 
 const changeMovieStarColor = (e) => {
   const currentStarId = e.target.id;
-  // const numOfStar = currentStarId.split('.')[0];
-  // const onlyNumOfStar = numOfStar.split('_')[1];
+  const numOfStar = currentStarId.split('.')[0];
+  const onlyNumOfStar = numOfStar.split('_')[1];
   const getClassName = document.getElementById(currentStarId).classList[0];
   if (getClassName === 'star') {
     document.getElementById(currentStarId).classList.replace('star', 'gray');
@@ -12,8 +12,14 @@ const changeMovieStarColor = (e) => {
     document.getElementById(currentStarId).classList.replace('gray', 'star');
   }
   // const getSecondClassName = document.getElementById(currentStarId).classList[1];
-  const test = document.getElementsByClassName('star');
-  console.error(test);
+  const test = document.getElementsByClassName('gray');
+  for (let i = 0; i < test.length; i += 1) {
+    if (test[i].id === currentStarId && onlyNumOfStar === '4') {
+      console.error(test[i].id, '....and num is....', onlyNumOfStar);
+      console.error('thisssssss', test[i]);
+      document.getElementById(`star_5.${currentStarId}`).classList.add('gray');
+    }
+  }
 };
 
 
