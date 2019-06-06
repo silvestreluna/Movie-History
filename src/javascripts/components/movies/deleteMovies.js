55ed488259fc4a7ea6175cd309f4abf4a3f34999
Silvestre Luna <slvtrluna@gmail.com>
2019-06-05 22:44:49 -0500

    wip

--- src/javascripts/components/movies/deleteMovies.js
+++ src/javascripts/components/movies/deleteMovies.js
@@ -5,7 +5,6 @@ import reprintMovies from './movies';
 const deleteMovie = (e) => {
   const currentId = e.target.closest('.each-card').id;
   const onlyCardId = currentId.split('.')[1];
-  console.error(onlyCardId);
   movieData.deleteMovie(onlyCardId)
     .then(() => {
       reprintMovies.initMoviesData();
